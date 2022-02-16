7c67ab5a2560d4914e71e986c501178785c2f8d8
Aaron Mackay <aaron.mackay@aquilaheywood.co.uk>
2022-02-16 09:51:54 +0000

    Test commit for CI

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
+
 Amplify.configure(config);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>,
+        document.getElementById('root')
 );
